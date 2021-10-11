3af449967cdb8abe90ab8ecaacae6b7e6f3382f5
Bei Su <bs3305@columbia.edu>
2021-10-11 23:28:28 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = params[:ratings]||{}
+    return @movies = Movie.with_ratings(params[:ratings])     
   end
 
   def new
